03a56b3345ee227f2f3b5522854fbcc6a9f18546
Beto Ayesa <contacto@phpninja.info>
2013-11-01 20:10:42 +0100

    templates

---
+++ public/views/templates/grid.php
@@ -0,0 +1,16 @@
+
+<div class="grid">
+
+     <ul class="thumbnails">
+<? foreach ($items as $item): ?>      
+  <li class="span3">
+    <div class="thumbnail">
+      <a href="detail.htm"><img src="http://placehold.it/260x180" alt=""></a>
+      <h5>Estantería IKEA</h5>
+      <p>40 €</p>
+    </div>
+	</li>
+	<? endforeach; ?>
+</ul>
+
+</div>
