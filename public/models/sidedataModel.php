9bdc73430c19774f7990486dcc40e3076c475e04
Beto Ayesa <contacto@phpninja.info>
2013-12-06 11:51:34 +0100

    sidedata blank

--- public/models/sidedataModel.php
+++ public/models/sidedataModel.php
@@ -8,12 +8,10 @@ class sidedataModel extends ModelBase
 
 	function load(){
 		
-		include "categorysModel.php";
-		include "juegoscategorysModel.php";
-		$cats =  new categorysModel();
-		$jcats = new juegoscategorysModel();
-		$sidedata = array("cats" => $cats->getAll(),
-			"jcats" => $jcats->getAll());
+		$sidedata = array(
+		
+		
+		);
 
 		
 		
