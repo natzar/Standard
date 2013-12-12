3d4fe3d4a0bd3ddb359c8fe2686c7b281bbb7d4d
Beto Ayesa <contacto@phpninja.info>
2013-12-12 14:15:08 +0100

    Chrome detection Alert

--- admin/views/includes/footer.php
+++ admin/views/includes/footer.php
@@ -13,6 +13,14 @@
 		var obj = $("a[href='"+cadena+"']");
 		obj.parent().addClass("active");
 
+
+		if (window.chrome){
+			$('#errors').html("OK Chrome detectado");
+			
+		}else{
+		
+			$('#errors').html("Mejor si usas Chrome <a href='//google.com/chrome'>Descárgalo</a>");
+		}
 	<? 	echo $HOOK_JS; ?>		</script>
 	
 	</body>
