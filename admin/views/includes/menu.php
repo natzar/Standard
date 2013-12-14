ded5d5cdc2512157b5c43806035f9551cd5b0928
Beto Ayesa <contacto@phpninja.info>
2013-12-14 01:56:27 +0100

    admin example menu

---
+++ admin/views/includes/menu.php
@@ -0,0 +1,23 @@
+<!--
+
+	<li class="nav-header">CAMPAÑAS</li>
+<li><a href="show/table/campanas">Campañas</a></li>
+-->
+
+
+
+	<li class="nav-header">BLOG</li>
+<li><a href="show/table/blogcategorys">Categorias</a></li>
+<li><a href="show/table/blog">Posts</a></li>
+
+
+<li class="nav-header">CONTENIDOS</li>
+<li><a href="show/table/contenidoscategorys">Secciones</a></li>	
+<li><a href="show/table/contenidossubcategorys">Categorias</a></li>	
+<li><a href="show/table/contenidos">Juegos y Actividades</a></li>
+<li><a href="show/table/personajes">Personajes</a></li>
+
+
+
+
+						
