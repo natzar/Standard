639613b1ec286b6442c11f294325e1d6312dff88
Beto Ayesa <contacto@phpninja.info>
2014-01-30 16:53:40 +0100

    users and permises setup files

---
+++ setup/tiposusuarios.php
@@ -0,0 +1,12 @@
+<?
+        
+        $table_label = "Usuarios TIpos";
+        $default_order = "id ASC";
+        $fields= array("tipo");
+        
+        $fields_labels= array("tipo");
+        
+        $fields_types=array("literal");
+        
+        
+        
---
+++ setup/users.php
@@ -0,0 +1,12 @@
+<?
+        
+        $table_label = "usuarios";
+        $default_order = "id ASC";
+        $fields= array("email","nombre","apellidos","password","tiposusuariosId");
+        
+        $fields_labels= array("email","nombre","apellidos","password","tipo Usuario");
+        
+        $fields_types=array("email","literal","literal","password","combo");
+        
+        
+        
