eb309088d437713e5207b71094f2d33cf3cc1ccc
Beto Ayesa <contacto@phpninja.info>
2014-05-18 18:57:05 +0200

    itwoorks rev

--- admin/views/includes/top.php
+++ admin/views/includes/top.php
@@ -34,12 +34,12 @@
 		</SCRIPT>
 		<script type="text/javascript" src="views/js/functions.js"></script>
 		<script src="views/js/pagination3.js"></script>
-		
+		<style>#accordion2 li{list-style: none;}</style>
 	</head>
 	<?php flush(); ?>
 	<body>
 	<div id="overlay" style="display:none;">Please wait ...</div>
-  <div class="navbar  navbar-fixed-top">
+  <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container-fluid">
           <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -62,24 +62,90 @@
       </div>
     </div>
 	<div class="container-fluid">
-		
-   
-
-
 	<div class="row-fluid">
-    <div class="span2">
-          <div class="well-small sidebar-nav" style="padding:1px">
-            <ul class="nav nav-list">
-				<? include "menu.php"; ?>          
-          </ul>
-         
-
-          </div><!--/.well -->
-               
-         <div id="errors" class="alert alert-info" style="margin-top:100px">
-         
-         </div>
-               
+    <div class="span2 new" style="">
+    	<div class="accordion" id="accordion2">
+				<div class="accordion-group">
+                  <div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#uno">
+                      Configuración
+                    </a>
+	                  </div>
+	                  <div id="uno" class="accordion-body in collapse">
+	                    <div class="accordion-inner">
+	                      <li><a href="show/table/config_meta"><i class=""></i> Configuración</a></li>
+						   <li><a href="show/table/fondos"><i class=""></i> Fondos</a></li>
+						   <li><a href="show/table/img_cabecera"><i class=""></i> Imagenes de Cabecera</a></li>
+						   <li><a href="show/table/pictos"><i class=""></i> Pictos</a></li>
+	                    </div>
+	                  </div>
+	                </div>
+				<div class="accordion-group">
+                  <div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#dos">
+                      Header
+                    </a>
+	                  </div>
+	                  <div id="dos" class="accordion-body collapse">
+	                    <div class="accordion-inner">
+	                      <li><a href="show/table/logo"><i class=""></i> Logo</a></li>
+						   <li><a href="show/table/social_media"><i class=""></i> Social Media</a></li>
+						   <li><a href="show/table/menu"><i class=""></i> Menu Cabecera</a></li>
+	                    </div>
+	                  </div>
+	                </div>
+				<div class="accordion-group">
+                  <div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#tres">
+                      Home
+                    </a>
+	                  </div>
+	                  <div id="tres" class="accordion-body collapse">
+	                    <div class="accordion-inner">
+	                      <li><a href="show/table/config_slider"><i class=""></i> Configuración Slider</a></li>
+						   <li><a href="show/table/slider"><i class=""></i> Slider</a></li>
+						   <li><a href="show/table/destacado"><i class=""></i> Destacado en la Home</a></li>
+	                    </div>
+	                  </div>
+	                </div>
+				<div class="accordion-group">
+                  <div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#cuatro">
+                      Productos
+                    </a>
+	                  </div>
+	                  <div id="cuatro" class="accordion-body collapse">
+	                    <div class="accordion-inner">
+	                      <li><a href="show/table/categoria"><i class=""></i> Categorias Productos</a></li>
+							   <li><a href="show/table/productos"><i class=""></i> Productos</a></li>
+	                    </div>
+	                  </div>
+	                </div>
+					<div class="accordion-group">
+                  <div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#cinco">
+                      Contenido Páginas
+                    </a>
+                  </div>
+                  <div id="cinco" class="accordion-body collapse">
+                    <div class="accordion-inner">
+                      	<li><a href="show/table/mundo"><i class=""></i> Mundo Vip Pets</a></li>
+						   <li><a href="show/table/news"><i class=""></i> News</a></li>
+						   <li><a href="show/table/padres"><i class=""></i> Padres</a></li>
+						   <li><a href="show/table/peinados"><i class=""></i> Galeria</a></li>
+						   <li><a href="show/table/personajes"><i class=""></i> Personajes</a></li>
+						   <li><a href="show/table/tutoriales"><i class=""></i> Tutoriales</a></li>
+						   <li><a href="show/table/videos"><i class=""></i> Videos</a></li>
+						   <li><a href="show/table/contacto"><i class=""></i> Contacto</a></li>
+						   <li><a href="show/table/footer"><i class=""></i> Footer</a></li>
+                    </div>
+                  </div>
+						   
+						   
+						  
+						   
+			        <!--  icon-chevron-right -->
+</div></div>
         </div><!--/span-->
 	
 		<div  id="main" class="span10" >
