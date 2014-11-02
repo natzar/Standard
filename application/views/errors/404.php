9c2cb80816a3c26082fe70d70d68a1012b237ad4
Beto Ayesa <contacto@phpninja.info>
2014-11-02 19:52:13 +0100

    404 page

--- application/views/errors/404.php
+++ application/views/errors/404.php
@@ -1,6 +1,9 @@
+<div class="container">
 <center>
 <br><br>
-<h1 style="font-size:80px">404</h1>
-<h2>NOT FOUND</h2>
-<a href="<?= $base_url?>">Index</a>
+<h1 style="font-size:180px;margin-bottom:0px">404</h1>
+<!-- <h2>NOT FOUND</h2> -->
+<h3>El contenido que buscas no está aquí</h3>
+<a href="<?= $base_url?>">Home Page</a>
+<br><br>
 </center>
