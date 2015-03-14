864a2d83aa7a9188e906f53277ea9de1f5d60a5e
Beto Ayesa <contacto@phpninja.info>
2015-03-14 09:44:51 +0700

    array with all tables involved to make a default menu

--- application/models/sidedataModel.php
+++ application/models/sidedataModel.php
@@ -6,14 +6,25 @@
 class sidedataModel extends ModelBase
 {
 
+	function tableList(){			
+        $config = Config::singleton();
+        
+        $dbname = $config->get('dbname');
+   
+        $consulta = $this->db->prepare('SHOW TABLES FROM '.$dbname);
+        $consulta->execute();
+        
+        $tableList = Array();
+        while ($row = $consulta->fetch(PDO::FETCH_NUM)) {
+        	$tableList[] = $row[0];        	
+        }
+        return $tableList;
+	}
 	function load(){
 		
 		$sidedata = array(
-		
-		
+			"tables" => $this->tableList()		
 		);
-
-		
 		
 		return $sidedata;
 	}
