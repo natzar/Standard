d9f021c1a32dd2e1355649bf2534cb3236287fb1
Beto Ayesa <contacto@phpninja.info>
2015-03-14 09:43:01 +0700

    empty admin dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,13 +1,2 @@
-<style>
-span.number{
-display: block;
-text-align: center;
-font-size: 150px;
-font-weight: bold;
-color:black;
-}
-small{text-align: center;display: block;font-size: 30px;color:black}
-</style>
 
-	<h1>Halloha</h1>
-	<p>dashboard.php</p>
+    <H1>ALLOHA</H1>
