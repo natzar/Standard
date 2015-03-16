4301ae5622a5370c6092725cb4af1762bd820269
Beto Ayesa <contacto@phpninja.info>
2015-03-16 13:05:14 +0700

    making developer work easy

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -22,7 +22,7 @@
 	</script>
 	
 	<!-- Custom Js -->
-	<script type="text/javascript" src="public/js/application.js"></script>
+	<script type="text/javascript" src="public/application.js"></script>
 
 	<!-- Google Analytics - UA Identifier defined at config.php -->
 	<script type="text/javascript"> var _gaq = _gaq || [];  _gaq.push(['_setAccount', '<?= $config->get('google_analytics-UA') ?>']);	  _gaq.push(['_trackPageview']);  (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 		    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);  })();		
