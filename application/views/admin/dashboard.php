2603062ce48c975b36a24bb7f46b769f820eb1d6
Beto Ayesa <contacto@phpninja.info>
2015-03-27 12:06:37 +0700

    dashboard with right debugging info

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,7 +1,53 @@
+<h1 class="page-header">Dashboard</h1>
+<? $nothing_to_show = true; ?>
+<? if (isset($_SESSION['errors']) and !empty($_SESSION['errors'])): ?>
+<div id="errors" class="alert alert-success">    <?= $_SESSION['errors'] ?>     </div>
+<? 
+unset($_SESSION['errors']);
+endif; ?>
+<? if ($config->get('developer_mode')): 
+	$nothing_to_show = false;
+?>
+  
+<div class="panel panel-info">
+<div class="panel-heading">
+Información del sistema <i class="glyphicon glyphicon-time"></i> <?= Date("H:i\h d/m/Y") ?>
+</div>
+
+<div class="panel-body">
+<div class="alert alert-info" role="alert">
+  <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+  <span class="sr-only">Atención:</span>
+Developer mode está activado en config.php
+</div>
 
 
-      
-                <h1 class="page-header">Dashboard</h1>
+
+
+	Php Version: <?= phpversion() ?><br>
+	POST_MAX: <?= ini_get('post_max_size')  ?><br>
+	Memory Limit: <?= ini_get('memory_limit')  ?><br>
+	<hr>
+	
+	Standart Version: <?= $config->get('version') ?> @ <?= $config->get('updated'); ?>
+	
+	<hr>
+	Permisos de escritura
+	
+	/setup/: <? if (is_writable($config->get('path')."/setup")) echo "YES";else echo "NO"; ?><br>
+	/data/: <? if (is_writable($config->get('path')."/data")) echo "YES";else echo "NO"; ?><br>
+	/data/img: <? if (is_writable($config->get('path')."/data/img")) echo "YES";else echo "NO"; ?><br>
+	/data/img/thumbs: <? if (is_writable($config->get('path')."/data/img/thumbs")) echo "YES";else echo "NO"; ?><br>
+	/application/: <? if (is_writable($config->get('path')."/application/controllers")) echo "YES";else echo "NO"; ?><br>
+<?	if (is_dir($config->get('path')."/tools")) echo 'Mejor eliminar la carpeta /tools si ya has terminado instalacion'; ?>
+<h2>Herramientas</h2>
+
+	 <a href="admin/tools/filldb" class="btn btn-info">Fill db with fake data</a>
+	<a href="admin/tools/makemodels" class="btn btn-success">Make Models, Controllers, Forms and Views</a>
+		
+</div>
+</div>
+<? endif; ?>
 
   
 <div class="panel panel-info">
@@ -10,13 +56,13 @@
 </div>
 
 <div class="panel-body">
-
+<? if ($nothing_to_show): ?>
     <div class="alert alert-success" role="alert">
   <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-  <span class="sr-only">Success:</span>
-	All systems are ready
+  <span class="sr-only">¡Genial!:</span>
+	Todo al día, bien configurado y funcionando
 </div>
-
+<? endif; ?>
  <div class="alert alert-info" role="alert">
   <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
   <span class="sr-only">Info:</span>
@@ -25,7 +71,7 @@
 
 
  
-      <div class="alert alert-danger" role="alert">
+<div class="alert alert-danger" role="alert">
   <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
   <span class="sr-only">Error:</span>
   Enter a valid email address
@@ -36,28 +82,3 @@
 </div>
 </div>
 
-
-  
-<div class="panel panel-info">
-<div class="panel-heading">
-Información del sistema
-</div>
-
-<div class="panel-body">
-
-	Version:<br>
-	POST_MAX:<br>
-	Memory Limit<br>
-	Mysql Version:
-	<hr>
-	Permisos de escritura
-<h2>Herramientas</h2>
-		<a href="index.php?action=makesetups&table=all" class="btn btn-primary">MakeSetups</a>
-
- <a href="index.php?action=filldb" class="btn btn-info">Fill db with fake data</a>
-
- 
-	<a href="index.php?action=makemodels" class="btn btn-success">Make Models, Controllers, Forms and Views</a>
-		
-</div>
-</div>
